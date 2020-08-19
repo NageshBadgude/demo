e53a589f3c9783b0d30b4dd34622f8f9c8944103
NageshBadgude <badgudenageh36@gmail.com>
2020-08-19 15:13:07 +0530

    modifed java program

--- jenkinsTest.java
+++ jenkinsTest.java
@@ -4,8 +4,7 @@ public class jenkinsTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Nagesh");
-
+		System.out.println("Yay we are getting this !");
 	}
 
 }
